3b62bde2049142c9339ced41acc5cafee3f5c0f2
Charles Bamford <cbamford@antoinesolutions.com>
2020-03-12 11:31:58 -0700

    Fix more config, move tests into src.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'd.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node", "d.ts"],
   globals: {
     "ts-jest": {
       babelConfig: true
     }
   },
   transform: {
-    "\\.js$": ['babel-jest']
-  }
+    "\\.js$": ["babel-jest"]
+  },
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules",
+    "<rootDir>/build"
+  ]
 };
-
