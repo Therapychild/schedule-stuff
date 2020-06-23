66c0dfc39c215ac6a3a594c6be619fa77a8cc0c8
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-06-23 12:52:35 -0700

    * Updated handle on the ScheduleAssign Action.

--- src/action_managers/resource/ScheduleAssign.ts
+++ src/action_managers/resource/ScheduleAssign.ts
@@ -2,12 +2,19 @@ import ReducibleActionManagerBase, {
   ReducibleActionManagerInterface
 } from "duckies/dist/action_managers/base/ReducibleActionManagerBase";
 import Resource from "duckies/dist/resource/Resource";
+import {
+  ScheduleSetActiveTimeEntry,
+  scheduleSetActiveTimeEntry
+} from "./ScheduleSetActiveTimeEntry";
+import {ScheduleSetActiveGroup} from "./ScheduleSetActiveGroup";
+import {UserListItem} from "../../components/UserListItem";
+import {JobListItem} from "../../components/JobListItem";
 
 export const scheduleAssign = "SCHEDULE_ASSIGN";
 
 export type Payload = {
-  timeEntry: Resource;
-  group: {id: ""};
+  group?: Resource;
+  timeEntry?: Resource;
 };
 
 export type ScheduleAssignAction = {
@@ -16,23 +23,18 @@ export type ScheduleAssignAction = {
 };
 
 /**
- * Assigns group.
- * If in job mode, group is job, and timeEntry is selected timeEntry
- * If in user mode, group is user, and timeEntry is selected timeEntry
+ * Assigns group and or timeEntry.
+ * If Payload has both a TimeEntry and a group, set the group to
+ * scheduleSetActiveTimeEntry
  *
+ * If Payload has a TimeEntry and an activeGroup in state, set group to
+ * scheduleSetActiveTimeEntry
+ *
+ * If there is an Group, and an activeTimeEntry in state, set timeEntry to
+ * scheduleSetActiveGroup.
  */
 export class ScheduleAssign extends ReducibleActionManagerBase
   implements ReducibleActionManagerInterface<Payload, ScheduleAssignAction> {
-
-  defaultState = {
-    timeEntry: {
-      id: "",
-      data: {
-        id: ""
-      }
-    },
-    group: {id: ""}
-  }
 
   /* istanbul ignore next */
   constructor() {
@@ -48,11 +50,29 @@ export class ScheduleAssign extends ReducibleActionManagerBase
   }
 
   handle(state: any, action: ScheduleAssignAction): void {
-    const { timeEntry, group } = action.payload;
+    let { group, timeEntry } = action.payload;
 
-    if(!(group.id in state.group)) {
+    // If payload does not provide a group or a timeEntry, do nothing.
+    if (!(group && timeEntry)) {
       return;
     }
-    timeEntry.data.id = state.group.id;
+
+    // If no group is provided from payload, get the activeGroup from state.
+    if (!group) {
+      const mode = state.viewMode;
+      const activeGroup = state.activeGroup;
+      if (mode === "user") {
+        group = state.resources.job[activeGroup]
+      }
+      else {
+        group = state.resources.user[activeGroup];
+      }
+    }
+
+    // If no timeEntry is provided in the payload, get the activeTimeEntry from state.
+    if (!timeEntry) {
+      timeEntry = state.resources.timeEntry[state.activeTimeEntry];
+    }
+    timeEntry.set(group.type, group)
   }
 }
