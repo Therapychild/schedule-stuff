847fe7e8d5f6d6b8b7de598d731bc300c1f63063
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-06-24 11:55:17 -0700

    * Changed the scheduleSetActiveGroup button from an HTML <button> to a Prime-React JSX <Button> component

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -14,7 +14,7 @@ export class UserListItem extends React.Component<Props, {}> {
     const { label, scheduleSetActiveGroup, scheduleAssign } = this.props
     return (
       <div className="p-clearfix">
-        <button style={{fontSize:'1em',float:'right',margin:'1em .5em 0 0'}} onClick={scheduleSetActiveGroup}>{label}</button>
+        <Button style={{fontSize:'1em',float:'right',margin:'1em .5em 0 0'}} onClick={scheduleSetActiveGroup}>{label}</Button>
         <Button className="assign" label="Assign" onClick={scheduleAssign} />
       </div>
     );
