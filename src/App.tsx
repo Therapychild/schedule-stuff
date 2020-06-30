68894467d2b2f45c200437c3ad84666b9d5b71d0
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-06-30 16:59:58 -0700

    * Updated the App.tsx by replacing the Timeline component with the ConnectedScheduleComponent. * Straightened up the imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 import React from "react";
-import { ConnectedBaseListBox } from "./components/connected/BaseListBox";
+import {ConnectedToggleButton} from "./components/connected/ToggleButton";
 import Sidebar from "components/dist/common/Sidebar";
-import { ConnectedToggleButton } from "./components/connected/ToggleButton";
-import TimeLine from "react-calendar-timeline";
+import {ConnectedBaseListBox} from "./components/connected/BaseListBox";
+import {ConnectedSchedule} from "./components/connected/Schedule";
 
 import "./App.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/nova-light/theme.css"
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
-
 import "./styles/sidebar.css";
 import "./styles/sidebar-toggle.css"
 import "./styles/timeline.css"
 import "./index.css"
 
-import Resource from "duckies/dist/resource/Resource";
-
 export default class App extends React.Component<{}, {}> {
-  static defaultProps = {
-    className: "",
-  };
 
   render() {
-    const {groups, items} = this.props;
 
     return (
       <div className="App">
         <ConnectedToggleButton label="Toggle" />
-        <TimeLine groups={groups} items={items}/>
+        <ConnectedSchedule />
         <Sidebar>
         <ConnectedBaseListBox />
       </Sidebar>
