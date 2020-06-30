1c3a3eef183e092c6a4766282002c6f1ddba78aa
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-06-30 16:52:25 -0700

    * Built out the TimeLineItemCard which composes a Card with a ConnectedTimeLineItem in the header, and other timeEntry data in the body.   * Body needs to be finished when the data to be displayed has been decided.

---
+++ src/components/TimeLineItemCard.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Card, { ExposedCardProps } from "components/dist/data_view/Card";
+import {ConnectedTimeLineItem} from "./connected/TimeLineItem";
+
+type Props = ExposedCardProps;
+
+export class TimeLineItemCard extends React.Component<Props, {}> {
+
+  header(): React.ReactNode {
+    const { data } = this.props;
+
+    console.log(data);
+    return (
+      <ConnectedTimeLineItem timeEntry={data} />
+    );
+  }
+
+  body(): React.ReactNode {
+    const { data } = this.props;
+
+    return (
+      <>
+        <div className="name">
+          <span>Name: </span>
+          {data.get("user.displayName")}
+        </div>
+        <div className="times">
+          <span>`Start: {data.get("start")}`</span>
+          <span>`End: {data.get("end")}`</span>
+        </div>
+      </>
+    );
+  }
+
+  footer(): React.ReactNode {
+    return null
+  }
+
+  render(): React.ReactNode {
+    const { data } = this.props;
+
+    return (
+      <Card
+        id={data.get("id")}
+        header={this.header()}
+        body={this.body()}
+        footer={this.footer()}
+        data={data}
+      />
+    );
+  }
+}
