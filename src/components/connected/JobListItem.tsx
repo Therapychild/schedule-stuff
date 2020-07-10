532ec955f036b8c4f0d2c12a3e884175e2cf2f7c
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-07-10 11:04:21 -0700

    * Created connected versions of the Job/UserListItem components.   * Added a dispatchToProps method to handle the onAssign and setActive methods on their unconnected counterparts.

---
+++ src/components/connected/JobListItem.tsx
@@ -0,0 +1,39 @@
+import {KeyValueAction} from "duckies/dist/action_managers/utility/KeyValue"
+
+import {
+  scheduleAssign,
+  ScheduleAssignAction
+} from "../../action_managers/resource/ScheduleAssign";
+
+import {
+  JobListItem,
+  DispatchProps,
+} from "../JobListItem";
+
+import Resource from "duckies/dist/resource/Resource";
+import {connect} from "react-redux";
+
+const mapDispatchToProps = (dispatch: Function): DispatchProps => {
+  return {
+    scheduleAssign: (resource?: Resource, timeEntry?: Resource): void => {
+      dispatch({
+        type: scheduleAssign,
+        payload: { resource, timeEntry },
+      } as ScheduleAssignAction);
+    },
+    scheduleSetActiveResource: (timeEntry: string): void => {
+      dispatch({
+        type: "KEY_VALUE",
+        payload: {
+          key: "scheduleSetActiveResource",
+          value: timeEntry
+        },
+      } as KeyValueAction);
+    },
+  };
+};
+
+export const ConnectedJobListItem = connect<{}, DispatchProps>(
+  null,
+  mapDispatchToProps
+)(JobListItem);
