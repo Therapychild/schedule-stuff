8dac6edc74a7f2fd0e71d4975d1d3a0b0c3367e4
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-07-14 15:18:49 -0700

    * Updated TimeLIneItem from dev * updated connected TimeLIneItem from dev

--- src/components/TimeLineItem.tsx
+++ src/components/TimeLineItem.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Button} from "primereact/button";
+import {Card as TimeEntryCard} from "time-entry/dist/components/Card";
 import {TMode} from "../types/mode";
 import Resource from "duckies/dist/resource/Resource";
 
 export interface DispatchProps {
-  scheduleSetActiveGroup: (group: string) => void;
-  scheduleSetActiveTimeEntry: (timeEntry: Resource) => void;
-  scheduleAssign: (group: Resource, timeEntry: Resource) => void;
-  scheduleViewTimeEntry: (timeEntry: Resource) => void;
+  scheduleAssign: (resource?: Resource, timeEntry?: Resource) => void;
+  scheduleSetActiveResource: (timeEntry: string) => void;
+  scheduleSetActiveTimeEntry: (timeEntry: string) => void;
+  scheduleViewTimeEntry: (timeEntry: string) => void;
 }
 
 export interface OwnProps {
@@ -16,33 +17,49 @@ export interface OwnProps {
 
 export interface StateProps {
   viewMode: TMode;
-  activeGroup: Resource;
+  activeResource: string;
+  activeTimeEntry: string;
+  activeViewedTimeEntry: string;
 }
 
 type Props = OwnProps & StateProps & DispatchProps;
 
 export class TimeLineItem extends React.Component<Props, {}> {
 
-  onSetActive() {
-    const {scheduleSetActiveTimeEntry, scheduleSetActiveGroup, timeEntry, viewMode} = this.props;
-    scheduleSetActiveTimeEntry(timeEntry.get("id"));
+  /**
+   * Sends the clicked timeEntry to have the activeResource assigned to it.
+   */
+  onAssign() {
+    const {scheduleAssign, timeEntry} = this.props;
 
-    // @todo When data structure is fixed, add .id to the user and job.
-    if (viewMode === "job") {
-      scheduleSetActiveGroup(timeEntry.get("user"));
-    }
-    else {
-      scheduleSetActiveGroup(timeEntry.get("job"));
-    }
+    scheduleAssign(timeEntry);
   }
 
-  onAssign() {
-    const {scheduleAssign, timeEntry, activeGroup} = this.props;
-    scheduleAssign(activeGroup, timeEntry);
+  /**
+   * The timeEntry is set to activeTimeEntry.
+   * If a resource is already assigned to the timeEntry, it is set to the
+   * activeResource.
+   */
+  onSetActive() {
+    const {
+      scheduleSetActiveResource,
+      scheduleSetActiveTimeEntry,
+      timeEntry,
+      viewMode
+    } = this.props;
+
+    if (timeEntry.get(`${viewMode}.id`)) {
+      scheduleSetActiveResource(timeEntry.get(`${viewMode}.id`));
+      scheduleSetActiveTimeEntry(timeEntry.get("id"));
+    }
+    else {
+      scheduleSetActiveTimeEntry(timeEntry.get("id"));
+    }
   }
 
   onViewTimeEntry() {
     const {scheduleViewTimeEntry, timeEntry} = this.props;
+
     scheduleViewTimeEntry(timeEntry.get("id"));
   }
 
@@ -52,13 +69,29 @@ export class TimeLineItem extends React.Component<Props, {}> {
       viewMode
     } = this.props;
     const id = timeEntry.get("id");
-    const title = viewMode === "job" ? timeEntry.get("user.displayName") : timeEntry.get("job.name");
+    let setActive = <></>;
+
+    /**
+     *  Show the SetActive Button, only if it has a name to display. This will
+     *  always be true in "user" viewMode because every timeEntry must have a
+     *  job assigned to it, not necessarily a user.
+     */
+    if (viewMode === "job") {
+      const user = timeEntry.get("user");
+      if (user) {
+        setActive = <Button className="time-entry" onClick={this.onSetActive} label={timeEntry.get("user.name")} />
+      }
+    }
+    else {
+      setActive = <Button className="time-entry" onClick={this.onSetActive} label={timeEntry.get("job.name")} />
+    }
 
     return (
       <div id={id} >
-        <Button className="time-entry" onClick={this.onSetActive} label={title} />
+        {setActive}
         <Button className="assign" label="Assign" onClick={this.onAssign} />
         <Button className="more-info" label="info" onClick={this.onViewTimeEntry} />
+        <TimeEntryCard data={timeEntry}/>
       </div>
     );
   }
