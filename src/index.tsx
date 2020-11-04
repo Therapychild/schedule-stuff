ccf7246a3f7ec0e3da380cb3c8be67a98501832f
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-11-04 14:09:24 -0800

    * Updated index.tsx   * Added the ApolloClient   * Added the CssBaseline component from MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import ScheduleApp from "./ScheduleApp";
 
+const client = new ApolloClient({
+  uri:
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    "/graphql/time-entry/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <ScheduleApp />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <CssBaseline />
+      <ScheduleApp />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 )
