8634325e44465967527ea12b25f31821e1472b05
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-11-10 15:34:48 -0800

    * Added the clientSchema

---
+++ src/util/clientSchema.ts
@@ -0,0 +1,121 @@
+import {gql} from "@apollo/client";
+
+export const GET_JOBS = gql`
+  query GetJobs($skillIds: [String]) {
+    getJobs(skillIds: $skillIds) {
+      uid
+      name
+      skills
+      releaseDate
+      dueDate
+      estimatedTime
+      scheduledTime
+      workedTime
+    }
+  }
+`;
+
+export const GET_SKILLS = gql`
+  query GetSkills {
+    getSkills {
+      uid
+      name
+    }
+  }
+`;
+
+export const SCHEDULE_GET_USERS = gql`
+  query ScheduleGetUsers($startTime: String!, $endTime: String!, $skillIds: String, $userName: String) {
+    scheduleGetUsers(startTime: $startTime, endTime: $endTime, skillIds: $skillIds, userName: $userName) {
+      uid
+      userName
+      skills
+      hoursScheduled
+      hoursWorked
+    }
+  }
+`;
+
+export const SCHEDULE_GET_TIME_ENTRIES = gql`
+  query ScheduleGetTimeEntries($startTime: String!, $endTime: String!, $userIds: String, $jobIds: String) {
+    scheduleGetTimeEntries(startTime: $startTime, endTime: $endTime, userIds: $userIds, jobIds: $jobIds) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
+
+export const NEW_TIME_ENTRY_JOB = gql`
+  mutation NewTimeEntryForJob($startTime: String!, $endTime: String!, $jobId: String!) {
+    newTimeEntryForJob(startTime: $startTime, endTime: $endTime, jobId: $jobId) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
+
+export const NEW_TIME_ENTRY_USER = gql`
+  mutation NewTimeEntryForUser($startTime: String!, $endTime: String!, $userId: String!) {
+    newTimeEntryForUser(startTime: $startTime, endTime: $endTime, userId: $userId) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
+
+export const MOVE_TIME_ENTRY = gql`
+  mutation MoveTimeEntry($startTime: String!, $endTime: String!, $timeEntryId: String!) {
+    moveTimeEntry(startTime: $startTime, endTime: $endTime, timeEntryId: $timeEntryId) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
+
+export const SET_USER = gql`
+  mutation SetUser($userId: String!, $timeEntryId: String!) {
+    setUser(userId: $userId, timeEntryId: $timeEntryId) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
+
+export const SET_Job = gql`
+  mutation SetJob($jobId: String!, $timeEntryId: String!) {
+    setJob(jobId: $jobId, timeEntryId: $timeEntryId) {
+      uid
+      job
+      user
+      startTime
+      duration
+      releaseDate
+      dueDate
+    }
+  }
+`;
