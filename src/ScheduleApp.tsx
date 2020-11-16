cc8a4550d8a63dd716860718597681fab399a0e4
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-11-16 08:48:13 -0800

    * Added the Toggle viewMode logic to the ScheduleApp component

--- src/ScheduleApp.tsx
+++ src/ScheduleApp.tsx
@@ -5,12 +5,22 @@ import {
 import {Sidebar} from "./components/Sidebar";
 import {Schedule} from "./components/Schedule";
 import {TMode} from "./types/mode";
+import {Button} from "@material-ui/core";
 
 export default function ScheduleApp() {
   const [{viewMode}, setViewMode] = useState<{ viewMode: TMode }>({viewMode: "job"});
 
+  function toggleViewMode() {
+    if(viewMode === "user") {
+      setViewMode({viewMode: "job"});
+    } else {
+      setViewMode({viewMode: "user"});
+    }
+  }
+
   return (
     <>
+      <Button onClick={() => {toggleViewMode()}}>{viewMode}</Button>
       <Sidebar viewMode={viewMode} setViewMode={setViewMode}/>
       <Schedule viewMode={viewMode} setViewMode={setViewMode}/>
     </>
