25842b7b43a853f0e90105f97f384d78db82cddb
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2020-11-18 09:23:04 -0800

    * Updated the Sidebar component   * Changed the variant type from "temporary" to "persistent" in order to add the ability to select items on the Schedule while the Sidebar is open   * Added a close Button   * Removed open state and toggle function, added ability to accept them as props * Updated the ScheduleApp   * Added the open state and toggle functionality from the sidebar and passed them to Sidebar   * Updated the Sidebar open button to a standard button, instead of an iconButton

--- src/ScheduleApp.tsx
+++ src/ScheduleApp.tsx
@@ -1,14 +1,16 @@
 import React, {useState} from "react";
-import {
-  makeVar
-} from "@apollo/client";
 import {Sidebar} from "./components/Sidebar";
 import {Schedule} from "./components/Schedule";
-import {TMode} from "./types/mode";
+import {TMode} from "./types/types";
 import {Button} from "@material-ui/core";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+
+declare const window: Window;
 
 export default function ScheduleApp() {
   const [{viewMode}, setViewMode] = useState<{ viewMode: TMode }>({viewMode: "job"});
+  const [{open}, setOpen] = useState({open: false});
 
   function toggleViewMode() {
     if (viewMode === "user") {
@@ -18,13 +20,34 @@ export default function ScheduleApp() {
     }
   }
 
+  const openSidebar = (): void => {
+    setOpen({open: true});
+  }
+
+  const closeSidebar = (): void => {
+    setOpen({open: false});
+  }
+
   return (
     <>
       <Button onClick={() => {
+        openSidebar();
+      }}>
+        {!open ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+      </Button>
+      <Button onClick={() => {
         toggleViewMode()
       }}>{viewMode}</Button>
-      <Sidebar viewMode={viewMode} setViewMode={setViewMode}/>
-      <Schedule viewMode={viewMode} setViewMode={setViewMode}/>
+      <Sidebar
+        open={open}
+        viewMode={viewMode}
+        setViewMode={setViewMode}
+        closeSidebar={closeSidebar}
+      />
+      <Schedule
+        viewMode={viewMode}
+        setViewMode={setViewMode}
+      />
     </>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,52 +1,46 @@
-import React, {useState, Fragment} from "react";
-import {TMode} from "../types/mode";
-import Slide from "@material-ui/core/Slide";
+import React from "react";
+import {TMode} from "../types/types";
 import Drawer from "@material-ui/core/Drawer";
-import {IconButton} from "@material-ui/core";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import {WindowedListBox} from "./WindowedListBox";
+import Button from "@material-ui/core/Button";
 
 // Drill unused props with lodash.
 export interface Props {
   children?: any;
   viewMode: TMode;
   setViewMode: Function;
+  open: boolean;
+  closeSidebar: Function;
 }
 
 export function Sidebar(props: Props) {
-  const {children, viewMode} = props;
-  const [open, setOpen] = useState(false);
+  const {children, viewMode, open, closeSidebar} = props;
 
-  const toggleSidebar = (): void => {
-    setOpen(!open);
-  }
-
-  // Opens to width of timeline column.
-  // Composes the BaseListBox which includes filters, and ListItems
   // Composes a Calendar widget along with filters (a popup when icon is pressed)
-
-  // @todo: If window width > xxx, variant is persistent, otherwise temporary.
-  const variant = "temporary";
-
-  // @todo: When mode is saved to cache, move button off of Sidebar to somewhere
-  // better suited.
+  // @todo Pass WindowList as a child, along with the filters.
   return (
-    <Fragment>
-      <IconButton onClick={() => {
-        toggleSidebar();
-      }}>
-        {!open ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
-      </IconButton>
+    <>
       <Drawer
         anchor={"left"}
         open={open}
         onClose={() => {
-          toggleSidebar();
+          closeSidebar();
         }}
         transitionDuration={{enter: 500, exit: 300}}
-        variant={variant}
+        variant={"persistent"}
+        ModalProps={{
+          disableAutoFocus: true,
+          disableRestoreFocus: true,
+          disableEnforceFocus: true,
+        }}
       >
+        <Button onClick={() => {
+          closeSidebar();
+        }}>
+          {!open ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+        </Button>
         <div
           style={{width: "100%", backgroundColor: "red", textAlign: "center"}}>
           Filters
@@ -54,6 +48,6 @@ export function Sidebar(props: Props) {
         {children}
         <WindowedListBox viewMode={viewMode}/>
       </Drawer>
-    </Fragment>
+    </>
   );
 }
